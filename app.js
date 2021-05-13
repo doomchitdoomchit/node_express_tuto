54bcadc25774cb763b8d432e600f1d224a5d21c2
doomchitdoomchit <wlsduf930@gmail.com>
2021-05-13 17:11:52 +0900

    async

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const http = require('http')
+const {readFile, writeFile } = require('fs').promises
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const server = http.createServer((req, res) => {
-    console.log('request event');
-    res.end('hello world')
-})
+// // const getText = (path) => {
+// //     return new Promise((resolve, reject)=>{
+// //         readFile(path, 'utf-8', (err, data)=>{
+// //             if(err){
+// //                 reject(err)
+// //             } else {
+// //                 resolve(data);
+// //             }
+// //         })
+// //     })
+// // }
 
-server.listen(5000, () => {
-    console.log('Server listening on port : 5000');
-})
+// // getText('./ch.8-11/content/first.txt')
+// //     .then(result => console.log(result))
+// //     .catch(err => console.log(err))
+
+const start = async () => {
+    try {
+        const first = await readFile('./ch.8-11/content/first.txt', 'utf8')
+        const second = await readFile('./ch.8-11/content/second.txt', 'utf8')
+        await writeFile('./content/result-mind-grenade.txt',
+        `THIS IS AWESOME : ${first} ${second}`,
+        {flag : 'a'}
+        )
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
